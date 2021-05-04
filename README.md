4294bd1f2308472b9bffd01379423f7ac6568d23
dorin.musteata <dorin.musteata@ebs-integrator.com>
2021-05-04 15:43:20 +0300

    setup services with env

--- README.md
+++ README.md
@@ -1,144 +1,54 @@
-# json2htmls
+# drf-ebs-utils
 
-**json2htmls** is a package that converts json (dict) to html tables.
-
+**drf-ebs-utils** is a package that offers service classes for notification-service , attachment-service , sso-service.
 
 ## Installation
 
-Use the package manager [pip](https://pip.pypa.io/en/stable/) to install json2htmls.
+```sh
+!! Package is not yet published !!
+````
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install drf_ebs_utils.
 
 ```bash
-pip install json2htmls
+pip install drf_ebs_utils
 ```
 
-## Available methods 
-```python
-json_to_html - converts json (dict) to html
-normalize - normalizes json (dict) , converts values to strings
-builder - main method that generates tables
-json_arrays_to_dictionary - converts array of objects to objects , with suffixes
-json_arrays_destroy - removes arrays from source
+Setting `.env`
+```shell
+cp .env.example .env
+
+SERVICE_SSO_DOMAIN=
+SERVICE_SSO_SERVICE_TOKEN=
+SERVICE_SSO_SECRET_TOKEN=
+SERVICE_SSO_SECRET_KEY=
+
+SERVICE_NOTIFICATION_HOST=
+SERVICE_NOTIFICATION_SECRET_KEY=
+
+SERVICE_ATTACHMENT_HOST=
+SERVICE_ATTACHMENT_SECRET_KEY=
+SERVICE_ATTACHMENT_SECRET_TOKEN=
 ```
+
+## Available service classes
+
+* SSOService
+* NotificationService
+* AttachmentService
 
 ## Usage
+
 Example
-
-```python
-from json2htmls import json_to_html
-import json
-with open('tests/sample.json') as json_file: # warning currently keys with values are not supported , e.g. "key": ["1", "2"]
-    data = json.load(json_file)
-source , html = json_to_html(data) # source is the dictionary , html is the html itself
 ```
+from src.services.sso import SSOService
+sso = SSOService()
 
-Result , use [htmlviewer](https://codebeautify.org/htmlviewer) to see the result
-```html
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Report</title>
-    <style>table { page-break-inside:auto } tr { page-break-inside:avoid; page-break-after:auto } thead { display:table-header-group } tfoot { display:table-footer-group } .pointer { display: block; clear: both; page-break-after: always; } .clearfix { overflow: auto; clear: both } .clearfix::after { content: &quot;&quot;; clear: both; display: table; } body { margin: 0 7rem; } table { width: 100%; font-size: 0.8rem; } th,td { border: 0.11rem solid black; } table, th,td { border-collapse: collapse; padding: 0.25rem 1rem; text-align: left; margin: 0; } tr:nth-child(even) { background-color: #cccccc61; } </style>
-  </head>
-  <body>
-    <table class="clearfix" id="table_id">
-      <tbody>
-        <div class="pointer"></div>
-        <h3>General</h3>
-        <tr>
-          <th>Id</th>
-          <td>0001</td>
-        </tr>
-        <tr>
-          <th>Type</th>
-          <td>donut</td>
-        </tr>
-        <tr>
-          <th>Name</th>
-          <td>Cake</td>
-        </tr>
-        <tr>
-          <th>Ppu</th>
-          <td>0.55</td>
-        </tr>
-        <tr></tr>
-      </tbody>
-    </table>
-    <table class="clearfix" id="table_batter" style="page-break-inside: avoid; margin-top: 2rem;">
-      <tbody>
-        <tr id="row_heading_batter_heading">
-          <td colspan="2">
-            <h3 class="clearfix">Batter - 4</h3>
-          </td>
-        </tr>
-        <tr id="row_heading_batter_0">
-          <th>Id</th>
-          <th>Type</th>
-        </tr>
-        <tr id="row_batter_0">
-          <td>1001</td>
-          <td>Regular</td>
-        </tr>
-        <tr id="row_batter_1">
-          <td>1002</td>
-          <td>Chocolate</td>
-        </tr>
-        <tr id="row_batter_2">
-          <td>1003</td>
-          <td>Blueberry</td>
-        </tr>
-        <tr id="row_batter_3">
-          <td>1004</td>
-          <td>Devil's Food</td>
-        </tr>
-      </tbody>
-    </table>
-    <h3>Batters</h3>
-    <table class="clearfix" id="table_batters">
-      <tbody></tbody>
-    </table>
-    <table class="clearfix" id="table_topping" style="page-break-inside: avoid; margin-top: 2rem;">
-      <tbody>
-        <tr id="row_heading_topping_heading">
-          <td colspan="2">
-            <h3 class="clearfix">Topping - 7</h3>
-          </td>
-        </tr>
-        <tr id="row_heading_topping_0">
-          <th>Id</th>
-          <th>Type</th>
-        </tr>
-        <tr id="row_topping_0">
-          <td>5001</td>
-          <td>None</td>
-        </tr>
-        <tr id="row_topping_1">
-          <td>5002</td>
-          <td>Glazed</td>
-        </tr>
-        <tr id="row_topping_2">
-          <td>5005</td>
-          <td>Sugar</td>
-        </tr>
-        <tr id="row_topping_3">
-          <td>5007</td>
-          <td>Powdered Sugar</td>
-        </tr>
-        <tr id="row_topping_4">
-          <td>5006</td>
-          <td>Chocolate with Sprinkles</td>
-        </tr>
-        <tr id="row_topping_5">
-          <td>5003</td>
-          <td>Chocolate</td>
-        </tr>
-        <tr id="row_topping_6">
-          <td>5004</td>
-          <td>Maple</td>
-        </tr>
-      </tbody>
-    </table>
-  </body>
-</html>
+from src.services.notification import NotificationService
+notification = NotificationService()
+
+from src.services.attachment import AttachmentService
+attachment = AttachmentService()
 ```
 
 ## Contributing
--- src/services/notification.py
+++ src/services/notification.py
@@ -1,39 +1,17 @@
 import json
 
-from functools import cached_property
-
-from typing import Union
-
-from django.db.models import Q
-from django.template.loader import render_to_string
-from django.utils import translation
 from rest_framework.exceptions import ValidationError
-
 from datetime import datetime
-
 from contextlib import suppress
-
-from applications.notifications.helpers import NotificationHelper
-from applications.settings.helpers import SettingHelper
-from applications.settings.models import Setting
-from applications.users.models import User
-from applications.users.helpers import RoleHelper
-
-from applications.commons.services.request import AbstractServiceProvider
-from applications.commons.services.storage import StorageService
-
-from applications.notifications.serializers.notification import NotificationSerializer
-from applications.notifications.models import Notification
-
-from applications.nomenclatures.models import NomenclatureAlertAddress
-
-from config.settings import SERVICE_NOTIFICATION_HOST, SERVICE_NOTIFICATION_SECRET_KEY
+from src.helpers.notification import NotificationHelper
+from src.services.abstract import AbstractServiceProvider
+from config.settings import (SERVICE_NOTIFICATION_HOST, SERVICE_NOTIFICATION_SECRET_KEY)
 
 
 # Create your services here.
 
 
-class NotificationService(StorageService, AbstractServiceProvider):
+class NotificationService(AbstractServiceProvider):
     URL_POST_WAITING = 'notification/waiting/'
     URL_POST_WAITING_BULK = 'notification/waiting/bulk/'
 
@@ -44,50 +22,17 @@ class NotificationService(StorageService, AbstractServiceProvider):
         self.key = SERVICE_NOTIFICATION_SECRET_KEY
         self.headers = self.default_headers
 
-        self.model = Notification
-        self.get_serializer_class = NotificationSerializer
-        self.create_serializer_class = NotificationSerializer
-        self.unique_identifier = 'code_name'
-        self.unique_identifiers = ['code_name', 'type', 'target_id']
-
-        # settings
-        self.settings_model = Setting
-        self.settings_helper = SettingHelper
-
         # Authorize
         self.auth()
 
-        # Locale
-        self.LOCALE = translation.get_language()
-
     @staticmethod
-    def get_template(path: str = 'mail/global', payload=None):
-        if payload is None:
-            payload = {}
-
-        return render_to_string(f'{path}.html', payload)
-
-    def get_model(self):
-        return self.model
-
-    def _set_method_payload(self, **kwargs):
+    def _set_method_payload(**kwargs):
         method = kwargs.get('method', 'email')
         recipient = kwargs.get('recipient', '')
         sender = kwargs.get('sender', '')
         subject = str(kwargs.get('subject', ''))
         body = kwargs.get('body', '')
         internal = kwargs.get('internal', False)
-
-        template = kwargs.get('template', {})
-        text = template.get('text', '')
-        html = template.get('html', {})
-        path = html.get('path', '') or 'mail/global'
-        html_payload = html.get('payload', {})
-
-        if html_payload:
-            body = self.get_template(path, html_payload)
-        elif text:
-            body = text
 
         # Compose payload
         payload = {
@@ -109,14 +54,6 @@ class NotificationService(StorageService, AbstractServiceProvider):
         :param kwargs:
         :return:
         """
-        # set locale
-        if recipient := kwargs.get('recipient', ''):
-            # set initial locale
-            self.LOCALE = translation.get_language()
-
-            # set user locale
-            self.set_locale(value=recipient)
-
         # forming payload
         dump = kwargs.get('dump', False)
         payload = self._set_method_payload(**kwargs)
@@ -125,10 +62,6 @@ class NotificationService(StorageService, AbstractServiceProvider):
         if metadata := kwargs.get('metadata', {}):
             metadata['title'] = str(metadata.get('title'))
             metadata['description'] = str(metadata.get('description'))
-
-        # restore payload
-        if recipient:
-            self.set_initial_locale()
 
         if dump or not payload.get('recipient'):
             return payload
@@ -166,7 +99,8 @@ class NotificationService(StorageService, AbstractServiceProvider):
 
         return
 
-    def _save(self, **kwargs):
+    @staticmethod
+    def _save(**kwargs):
         response = kwargs.get('response', {})
         delivery_method = response.get('delivery_method', 'email').upper()
         metadata = kwargs.get('metadata', {})
@@ -177,9 +111,10 @@ class NotificationService(StorageService, AbstractServiceProvider):
             'description': response.get('message_body', ''),
             'read': response.get('is_read', False),
             'timestamp': response.get('created_at', datetime.now()),
+            'delivery_method': delivery_method,
             'edited_timestamp': response.get('modified_at', datetime.now()),
-            'target_id': self._get_user_by(delivery_method, response.get('recipient', '')),
-            'user_id': self._get_user_by(delivery_method, response.get('sender', '')),
+            'recipient': response.get('recipient', ''),
+            'sender': response.get('sender', ''),
             'code_name': response.get('id', ''),
             'type': response.get('delivery_method', 'email').upper(),
             'data': response,
@@ -187,122 +122,8 @@ class NotificationService(StorageService, AbstractServiceProvider):
             'metadata': metadata
         }
 
-        return self.upsert(data=output)
-
-    def _get_user_by(self, method: str, data: str) -> Union[int, None]:
-        """
-        Get user pk by key
-        :param method: Method to check
-        :param data: Email or Phone
-        :return:
-        """
-        if method not in [self.model.TYPE_SMS, self.model.TYPE_EMAIL]:
-            return None
-
-        key = 'email'
-        if method == self.model.TYPE_SMS:
-            key = 'phone'
-
-        queryset = User.objects.filter(**{key: data})
-        return queryset.values_list("id", flat=True).first()
-
-    @cached_property
-    def alerts(self) -> Union[list, None]:
-        """
-        Get alert addresses
-        :return:
-        """
-        return NomenclatureAlertAddress.objects.all()
-
-    @staticmethod
-    def alerts_roles(roles=None) -> Union[list, None]:
-        """
-        Get alert addresses by roles
-        :return:
-        """
-        if not roles:
-            roles = [RoleHelper.SY_CREDIT_OFFICER_SUPERIOR]
-
-        return User.objects.filter(groups__name__in=roles)
-
-    def alerting_roles(self, **payload) -> Union[list, None]:
-        """
-        Send notification payloads to alert addresses by roles
-        :return:
-        """
-        alerts = self.alerts_roles(payload.get('roles', []))
-
-        if not alerts:
-            return
-
-        # Send notifications
-        for alert in alerts:
-            payload.update({'recipient': alert.email})
-            self.send(**payload)
-
-        return
-
-    def alerting(self, **payload) -> Union[list, None]:
-        """
-        Send notification payloads to alert addresses
-        :return:
-        """
-        alerts = self.alerts
-
-        if not alerts:
-            return
-
-        # Send notifications
-        for alert in alerts:
-            email = alert.user.email if alert.user else alert.email
-            payload.update({'recipient': email})
-            self.send(**payload)
-
-        return
-
-    def alerting_bulk(self, notifications):
-        """
-        Send notification payloads to alert addresses
-        :return:
-        """
-        alerts = self.alerts
-
-        if not alerts:
-            return
-
-        # Send notifications
-        for alert in alerts:
-            email = alert.user.email if alert.user else alert.email
-
-            for notification in notifications:
-                notification.update({'recipient': email})
-
-            self.bulk_send(notifications)
-
-        return
+        return output
 
     def auth(self):
         self.headers['Authorization'] = f'Bearer {self.key}'
         return self
-
-    @staticmethod
-    def set_locale(user: User = None, value: str = None):
-        # get user
-        if not user and value:
-            user = User.objects.filter(Q(email=value) | Q(phone=value)).first()
-
-        # validate
-        if not isinstance(user, User):
-            return
-
-        # get locale
-        if not (locale := getattr(user, 'locale', None)):
-            return
-
-        # set locale
-        translation.activate(locale)
-
-    def set_initial_locale(self):
-        if self.LOCALE == translation.get_language():
-            return
-        translation.activate(self.LOCALE)
